6f88bde522e595fc3b7887472c95e7eb733be693
Andreas Finger <andreas-finger@cookpad.com>
2018-10-02 15:27:17 +0100

    Hooray

--- tcr_spec.rb
+++ tcr_spec.rb
@@ -2,7 +2,7 @@ require "rspec"
 
 class Tcr
   def self.fib(n)
-    return 1
+    return 1 if n < 3
   end
 end
 
@@ -10,5 +10,6 @@ describe Tcr do
   it "test" do
     expect(described_class.fib(1)).to eq(1)
     expect(described_class.fib(2)).to eq(1)
+    # expect(described_class.fib(3)).to eq(2)
   end
 end
