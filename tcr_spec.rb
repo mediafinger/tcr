60a57d0d7c7ac77e1a771a77feb7de6d644dd895
Andreas Finger <andreas-finger@cookpad.com>
2018-10-02 15:25:47 +0100

    Hooray

--- tcr_spec.rb
+++ tcr_spec.rb
@@ -1,6 +1,13 @@
 require "rspec"
-require_relative "tcr.rb"
+
+class Tcr
+  def self.fib(n)
+    return 1
+  end
+end
 
 describe Tcr do
-
+  it "test" do
+    expect(described_class.fib(1)).to eq(1)
+  end
 end
