95f9d75dd893e15376f1d13f7eba91743b6160ac
Andreas Finger <webmaster@mediafinger.com>
2018-10-17 19:03:26 +0200

    Hooray

--- tcr_spec.rb
+++ tcr_spec.rb
@@ -1,18 +1,6 @@
 require "rspec"
 require_relative "./tcr.rb"
 
-class Tcr
-  def self.fib(n)
-    return 1 if n < 3
-
-    fib(n - 1) + fib(n -2)
-  end
-
-  def self.square(n)
-    n * n
-  end
-end
-
 describe Tcr do
   it "tests fibonacci" do
     expect(described_class.fib(1)).to eq(1)
