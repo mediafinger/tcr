acc1d50896d27a8e7151f57da808e52ae03953ac
Andreas Finger <andreas-finger@cookpad.com>
2018-11-15 21:17:13 +0100

    Fibonacci

--- fibonacci_spec.rb
+++ fibonacci_spec.rb
@@ -16,5 +16,6 @@ describe Fibonacci do
     expect(described_class.fib(2)).to eq(1)
     expect(described_class.fib(3)).to eq(2)
     expect(described_class.fib(4)).to eq(3)
+    expect(described_class.fib(5)).to eq(5)
   end
 end
