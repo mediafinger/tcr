9946ee89799612a3bfe557c70444085b9e22d175
Andreas Finger <andreas-finger@cookpad.com>
2018-11-15 21:16:35 +0100

    Fibonacci

--- fibonacci_spec.rb
+++ fibonacci_spec.rb
@@ -2,13 +2,16 @@ require "rspec"
 
 class Fibonacci
   def self.fib(n)
-    return 1 if n == 1 || n == 2
-    2
+    return 0 if n == 0
+    return 1 if n == 1
+
+    fib(n - 1) + fib(n - 2)
   end
 end
 
 describe Fibonacci do
   it "tests" do
+    expect(described_class.fib(0)).to eq(0)
     expect(described_class.fib(1)).to eq(1)
     expect(described_class.fib(2)).to eq(1)
     expect(described_class.fib(3)).to eq(2)
